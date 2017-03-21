4be58608e671044f9ae646917217a68e952cc4f9
laden666666 <njz198869@gmail.com>
2017-03-21 20:37:50 +0800

    改用webpack的umd打包

---
+++ test.js
@@ -0,0 +1,5 @@
+/**
+ * Created by njz on 2017/3/21.
+ */
+var i = require("./dist/vue-scroller.min");
+console.log(i)
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './docs'),
     publicPath: '/dist/',
+    libraryTarget: "umd",
     filename: 'build.js'
   },
   module: {
